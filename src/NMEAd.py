1f6548aaea6ccf64c747f15cdfd9088639f5aed9
Fran Borcic <fborcic@gmail.com>
2016-02-14 17:51:04 +0100

    Removed file_lock definition from NMEAd.py

--- src/NMEAd.py
+++ src/NMEAd.py
@@ -33,24 +33,6 @@ SERIAL_TIMEOUT = 0.05
 
 terminate = False   # Global termination flag
 
-@contextmanager
-def file_lock(filed, exclusive=False):
-    """Non-blocking file lock context manager"""
-    opcode = [fcntl.LOCK_SH, fcntl.LOCK_EX][exclusive] | fcntl.LOCK_NB
-    try:
-        try:
-            fcntl.flock(filed, opcode)
-        except IOError, err:
-            if not (err.errno == errno.EAGAIN or err.errno == errno.EACCES):
-                raise
-            else:
-                yield False
-        else:
-            yield True
-    except:
-        raise
-    finally:
-        fcntl.flock(filed, fcntl.LOCK_UN)
 
 def thunk(callable_, *args, **kwargs):
     """Return thunk that calls callable_ with args/kwargs"""
